2a996308b9121791de07efb55878b885569abf26
haoflynet <haoflynet@gmail.com>
2014-01-23 20:45:11 +0800

    another bug

--- plugins/post_footer_filter.rb
+++ plugins/post_footer_filter.rb
@@ -12,11 +12,7 @@ module AppendFooterFilter
      author = post.site.config['author']
      url = post.site.config['url']
      pre = post.site.config['original_url_pre']
-     post.content + %Q[<p class='post-footer'>
-            #{pre or "original link:"}
-            <a href='#{post.full_url}'>#{post.full_url}</a><br/>
-            版权声明：自由转载-非商用-非衍生-保持署名| <a href='http://creativecommons.org/licenses/by-nc-nd/3.0/deed.zh'>Creative Commons BY-NC-ND 3.0</a>
-            </p>]
+     post.content + %Q[<p class='post-footer'>&nbsp;#{pre or "原文地址："}<a href='#{post.full_url}'>#{post.full_url}</a><br/>&nbsp;版权声明：自由转载-非商用-非衍生-保持署名| <a href='http://creativecommons.org/licenses/by-nc-nd/3.0/deed.zh'>Creative Commons BY-NC-ND 3.0</a></p>]
   end 
 end
 
